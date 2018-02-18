bc64429e91c963f25f337a815a717421ff735c60
James Kent <james-kent@uiowa.edu>
2018-02-18 02:02:56 -0600

    [FIX] make the test run correctly on the first commit

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 
-changed_sh_files=$(git diff --name-only "${TRAVIS_COMMIT_RANGE}" | grep .sh)
+changed_sh_files=$(git diff --name-only HEAD~1 | grep .sh)
 if [ -z "${changed_sh_files}" ]; then
   echo "no shell scripts were changed in this pull request"
 else
