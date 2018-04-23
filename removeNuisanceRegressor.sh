0548f280a4facb10ea6ac3ba9430562cb83b14dd
jdkent <james-kent@uiowa.edu>
2018-04-22 21:37:10 -0400

    temporarily more verbose and a hacky (partial) fix if --compor is not specified

--- removeNuisanceRegressor.sh
+++ removeNuisanceRegressor.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 ##################################################################################################################
 # Removal of Nuisance Signal, Filtering
@@ -218,7 +218,11 @@ while [ $# -ge 1 ] ; do
     ?)
       echo "ERROR: Invalid option"
       printCommandLine
+      shift;;
       ;;
+    *)
+      echo "oops"
+      shift;;
      esac
 done
 
@@ -293,8 +297,8 @@ done
 {
 echo "------------------------------------"
 echo "-E $epiData"
-echo "-A $t1Data" 
-echo "-N $nuisanceInFile" 
+echo "-A $t1Data"
+echo "-N $nuisanceInFile"
 echo "-L $lp"
 echo "-H $hp"
 } >> "$logDir"/rsParams_log
@@ -304,7 +308,7 @@ if [[ $overwriteFlag == 1 ]]; then
 fi
 {
 date >> "$logDir"/rsParams_log
-echo -e "\\n\\n" 
+echo -e "\\n\\n"
 } >> "$logDir"/rsParams_log
 
 
