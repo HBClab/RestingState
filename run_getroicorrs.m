2fe36ba451170229c6a89908077a11f62c70cc13
mwvoss <michelle-voss@uiowa.edu>
2018-04-28 23:13:58 -0400

    WIP: updated paths in run_getroicorrs.m

--- run_getroicorrs.m
+++ run_getroicorrs.m
@@ -1,5 +1,5 @@
 % This is a wrapper script that calles the actual matlab script get_fc_matrix_subset.m that does
-% the actual correlation computations. Save these two scripts in your current working folder or 
+% the actual correlation computations. Save these two scripts in your current working folder or
 % where you start matlab from or in a place that is in your matlab's search
 % path and run this code after making necessary changes to your path
 % structure. Read on...
@@ -11,7 +11,7 @@ clear all;
 %Specify a text file that has all the subjects listed one per line. (It is better
 % if you can give the full path).
 
-fid1=fopen('/Users/suttererm/VossLabMount/Projects/FAST/PreprocData/n189_time_2_raw_exists.txt');
+fid1=fopen('/home/brain/Labs/12-Lab/sublist.txt');
 subList_tmp=textscan(fid1,'%s');fclose(fid1);
 N=length(subList_tmp{1,1});
 subList=cell(N,1);
@@ -22,7 +22,7 @@ end
 % Please specify a textfile that has all your regions of interest listed one per line. (It is better
 % if you can give the full path).
 
-fid2=fopen('/Users/suttererm/VossLabMount/Projects/FAST/PreprocData/scripts/fast_UIUC_DMN_phgf.txt');   %ROI LIST HERE
+fid2=fopen('/home/brain/Labs/12-Lab/ROIs/roilist.txt');   %ROI LIST HERE
 roiList_tmp=textscan(fid2,'%s');fclose(fid2);
 N1=length(roiList_tmp{1,1});
 roiList=cell(N1,1);
@@ -38,13 +38,13 @@ end
 % get_fc_matrix_subset.m script at line 66. If this part is different for you, please change it according to
 % your directory structure. Ultimately, it should point to the _residvol_ts.txt
 
-mypwd='/Users/suttererm/VossLabMount/Projects/FAST/PreprocData/';
+mypwd='/home/brain/Labs/12-Lab/';
 
 % if your data is motion scrubbed, use motion_scrub=1 else use
 % motion_scrub=0. If motion scrubbed; it will look for
 % roi_residvol_ms_ts.txt in the location: mypwd/subname/image/rest/nuisancereg.feat/stats/
 
-motion_scrub=1; 
+motion_scrub=1;
 
 % This specifies the number of time points in the functional data. Change
 % it according to your data.
@@ -59,12 +59,12 @@ numvols=180;
 % if you are familiar with matlab, then:
 % -the matrix corrlist_subs_ms gives you the correlation of each subject to each roi-pairs.
 % -the matrix avgcorrmat_subs_ms gives you the cross-correlation of roi time-series averaged over all subjects.
-%    
-% if do not like to work with matlab, the above matrices are written to some text files for your convenience. 
+%
+% if do not like to work with matlab, the above matrices are written to some text files for your convenience.
 % Look for the text files 'subject_roi-pair_corr' and 'roi-roi_corr' in your current working directory.
 
 % the following code creates a text file 'subject_roi-pair_corr' in your current directory with the correlation values for each subject
-% to each roi-pairs 
+% to each roi-pairs
 
 pair=[char(9)];roi_row=[char(9)];
 for roi1=1:length(roiList)
@@ -81,14 +81,15 @@ end
 if(exist('subject_roi-pair_corr'))
     delete('subject_roi-pair_corr');
 end
-fid = fopen('subject_roi-pair_corr_compcor_n189_ms_UIUC_DMN_phgf_time_2.txt','w');
+
+fid = fopen('subject_roi-pair_corr.txt','w');
 fprintf(fid, '%s \n', pair);
 fclose(fid);
 % the following code creates a text file 'roi-roi_corr' in your current directory with the cross-correlation
 % of rois averaged over all subjects
 
 for row=1:N
-    fid = fopen('subject_roi-pair_corr_compcor_n189_ms_UIUC_DMN_phgf_time_2.txt','a');
+    fid = fopen('subject_roi-pair_corr.txt','a');
     clear a;
     a=char(num2str(corrlist_subs_ms(row,:)));
     fprintf(fid,'%s %s \n',['sub',char(subList{row,1}),'_2'],a);
@@ -99,7 +100,7 @@ if(exist('roi-roi_corr'));
     delete('roi-roi_corr');
 end
 
-fid = fopen('roi-roi_corr_compcor_n189_ms_UIUC_DMN_phgf_time_2.txt','a');
+fid = fopen('roi-roi_corr.txt','a');
 fprintf(fid, '%s \n', roi_row);
 fclose(fid);
 
@@ -108,8 +109,7 @@ for roi1=1:length(roiList)
     r2=strrep(r1,'.nii.gz',' ');
     clear a;
     a=char(num2str(avgcorrmat_subs_ms(roi1,:)));
-    fid = fopen('roi-roi_corr_compcor_n189_ms_UIUC_DMN_phgf_time_2.txt', 'a');
+    fid = fopen('roi-roi_corr.txt', 'a');
     fprintf(fid, '%s %s \n', char(r2),a);
     fclose(fid);
 end
-
