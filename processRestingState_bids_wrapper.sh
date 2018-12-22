08d881136845e4a2cda6d0e30090d2529b82fd2f
mwvoss <michelle-voss@uiowa.edu>
2018-12-22 08:45:24 -0600

    add ses_anat and test whether epi and t1 are from the same ses

--- processRestingState_bids_wrapper.sh
+++ processRestingState_bids_wrapper.sh
@@ -363,10 +363,29 @@ if [[ "${roilist}" == "" ]]; then
 fi
 
 bidsDir="${inFile//\/sub*}" # bids directory e.g., /vosslabhpc/Projects/Bike_ATrain/Imaging/BIDS
-subID="$(echo "${inFile}" | grep -o "sub-[a-z0-9A-Z]*" | head -n 1 | sed -e "s|sub-||")" # gets subID from inFile
-sesID="$(echo "${inFile}" | grep -o "ses-[a-z0-9A-Z]*" | head -n 1 | sed -e "s|ses-||")" # gets sesID from inFile
+subID="$(echo "${inFile}" | grep -o "sub-[a-z0-9A-Z]*" | head -n 1 | sed -e "s|sub-||")" # gets subID from epi
+sesID="$(echo "${inFile}" | grep -o "ses-[a-z0-9A-Z]*" | head -n 1 | sed -e "s|ses-||")" # gets sesID from epi
+sesID_anat="$(echo "${t1}" | grep -o "ses-[a-z0-9A-Z]*" | head -n 1 | sed -e "s|ses-||")" # gets sesID from t1
 subDir="${bidsDir}/sub-${subID}" # e.g., /vosslabhpc/Projects/Bike_ATrain/Imaging/BIDS/sub-GEA161
 scanner="$(echo "${subID}" | cut -c -2)" # extract scannerID from subID, works when scannerID is embedded in subID. TODO: need a different way to determine scannerID. e.g., dicom header?
+
+# test if sesID blank
+if [[ "${sesID}" == "" ]]; then
+	echo "no sesID"
+	rsOut="${bidsDir}/derivatives/rsOut/sub-${subID}"
+	rsOut_anat="${bidsDir}/derivatives/rsOut/anat/sub-${subID}"
+elif
+	echo "session is ${sesID}"
+	# test whether epi and t1 are from the same session
+	if [ "${sesID}" == "${sesID_anat}" ]; then
+		rsOut="${bidsDir}/derivatives/rsOut/sub-${subID}/ses-${sesID}"
+		rsOut_anat="${bidsDir}/derivatives/rsOut/anat/sub-${subID}/ses-${sesID}"
+	elif
+		rsOut="${bidsDir}/derivatives/rsOut/sub-${subID}/ses-${sesID}"
+		rsOut_anat="${bidsDir}/derivatives/rsOut/anat/sub-${subID}/ses-${sesID_anat}"
+	fi
+fi
+
 rsOut="${bidsDir}/derivatives/rsOut/sub-${subID}/ses-${sesID}"
 rsOut_anat="${bidsDir}/derivatives/rsOut/anat/sub-${subID}/ses-${sesID}"
 mkdir -p "${rsOut_anat}"
