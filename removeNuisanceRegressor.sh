495e13aac8e5567780ebe37e9b111d5ad9b8578a
mwvoss <michelle-voss@uiowa.edu>
2019-02-18 05:58:48 -0600

    try to fix paths for nuisance rois and add clobber since rois are running through roi loop

--- removeNuisanceRegressor.sh
+++ removeNuisanceRegressor.sh
@@ -365,10 +365,13 @@ do
     fslmeants -i "$epiData" -o "$outDir"/rois/mean_"${roiName}"_ts.txt -m "$outDir"/rois/"${roiName}"_native.nii.gz --eig --order=5
   elif [[ "${compcor_globalFlag}" -eq 1 ]]; then
     # mean for global
+    clobber "$outDir"/rois/mean_global_ts.txt &&\
     fslmeants -i "$epiData" -o "$outDir"/rois/mean_global_ts.txt -m "$outDir"/rois/global_native.nii.gz
     # eigenvariates for WM and CSF
-    fslmeants -i "$epiData" -o "$outDir"/rois/mean_CSF_pve_to_RS_thresh_ts.txt -m "$rawEpiDir"/SNR/CSF_pve_to_RS_thresh.nii.gz --eig --order=5
-    fslmeants -i "$epiData" -o "$outDir"/rois/mean_WM_pve_to_RS_thresh_ero_ts.txt -m "$rawEpiDir"/SNR/WM_pve_to_RS_thresh_ero.nii.gz --eig --order=5
+    clobber "$outDir"/rois/mean_CSF_pve_to_RS_thresh_ts.txt &&\
+    fslmeants -i "$epiData" -o "$outDir"/rois/mean_CSF_pve_to_RS_thresh_ts.txt -m "$outDir"/rois/CSF_pve_to_RS_thresh_native.nii.gz --eig --order=5
+    clobber "$outDir"/rois/mean_WM_pve_to_RS_thresh_ero_ts.txt &&\
+    fslmeants -i "$epiData" -o "$outDir"/rois/mean_WM_pve_to_RS_thresh_ero_ts.txt -m "$outDir"/rois/WM_pve_to_RS_thresh_ero_native.nii.gz --eig --order=5
   else
     clobber "$outDir"/rois/mean_"${roiName}"_ts.txt &&\
     fslmeants -i "$epiData" -o "$outDir"/rois/mean_"${roiName}"_ts.txt -m "$outDir"/rois/"${roiName}"_native.nii.gz
