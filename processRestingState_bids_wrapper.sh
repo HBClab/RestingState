3dfc7b32558f24d00cc6d26a5be3b7784883746c
James Kent <james-kent@uiowa.edu>
2019-02-26 11:43:38 -0600

    [MAINT] remove freesurfer from Dockerfile (#106)
    * remove freesurfer from Dockerfile
    
    * bump the version for the docker cache
    
    * remove check for freesurfer

--- processRestingState_bids_wrapper.sh
+++ processRestingState_bids_wrapper.sh
@@ -52,12 +52,6 @@ function softwareCheck()
       echo "fsl is either not downloaded or not defined in your path, exiting script"
       exit 1
   fi
-
-  if [ "$(which freesurfer)" == "" ]; then
-      echo "freesurfer is either not downloaded or not defined in your path, exiting script"
-      exit 1
-  fi
-
 }
 
 function clobber()
